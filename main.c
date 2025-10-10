e3b85ec1bcc99940d474622158508063300f6ac6
gerrardidemi <gerrardidemi45@gmail.com>
2025-10-10 17:30:15 +0100

    I just added info into the main.c file

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+float a = 10;
+float b = 3;
+float answer = 10/3;
+printf("My name is Idemi Gerrard\n");
+printf("I am %d years old\n" , 15);
+printf("no %d redeeme street okpanam road\n" ,1);
+printf("The first letter  of my country is %c\n" ,'n');
+printf("10 divided by 3 is = %.2f\n" , answer);
+
+
+return 0;
+}
